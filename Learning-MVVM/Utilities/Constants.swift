c294ae432df53be9dd2624f220a2eff7db98a87c
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-20 12:10:15 +0700

    feat: add constants

---
+++ Learning-MVVM/Utilities/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Learning-MVVM
+//
+//  Created by Nuttapon Buaban on 20/6/2565 BE.
+//
+
+import Foundation
+
+enum Constants {
+    static let demoURL = "https://raw.githubusercontent.com/johncodeos-blog/MVVMiOSExample/demo.json"
+}
+
+enum HTTPHeaderFields {
+    case application_json
+    case application_x_www_form_urlencoded
+    case none
+}
+
+enum HTTPMethods: String {
+    case POST, GET, PUT, DELETE, PATCH
+}
